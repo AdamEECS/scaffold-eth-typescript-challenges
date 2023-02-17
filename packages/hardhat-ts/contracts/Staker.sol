98cdaad670c4a24f01387276306c618502163dee
suzumiya <jrgwyz@163.com>
2023-02-17 14:14:56 +0800

    Checkpoint 3: 🔬 State Machine / Timing ⏱

--- packages/hardhat-ts/contracts/Staker.sol
+++ packages/hardhat-ts/contracts/Staker.sol
@@ -10,6 +10,8 @@ contract Staker {
   mapping(address => uint256) public balances;
   uint256 public constant threshold = 1 ether;
   uint256 public deadline = block.timestamp + 30 seconds;
+  bool public openForWithdraw = false;
+  bool public executed = false;
 
   event Stake(address indexed sender, uint256 amount);
 
@@ -27,10 +29,26 @@ contract Staker {
 
   // TODO: After some `deadline` allow anyone to call an `execute()` function
   //  It should call `exampleExternalContract.complete{value: address(this).balance}()` to send all the value
+  function execute() public {
+    require(block.timestamp >= deadline, 'Deadline not reached');
+    require(!executed, 'Already executed');
+    executed = true;
+    if (address(this).balance < threshold) {
+      openForWithdraw = true;
+    } else {
+      exampleExternalContract.complete{value: address(this).balance}();
+    }
+  }
 
   // TODO: if the `threshold` was not met, allow everyone to call a `withdraw()` function
 
   // TODO: Add a `timeLeft()` view function that returns the time left before the deadline for the frontend
+  function timeLeft() public view returns (uint256) {
+    if (block.timestamp >= deadline) {
+      return 0;
+    }
+    return deadline - block.timestamp;
+  }
 
   // TODO: Add the `receive()` special function that receives eth and calls stake()
 }
