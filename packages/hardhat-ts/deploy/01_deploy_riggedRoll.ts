c850300909bde294e8d5d2cc85075b6acced6d7f
suzumiya <jrgwyz@163.com>
2023-02-18 11:32:55 +0800

    Checkpoint 4: 💵 Where's my money?!?

--- packages/hardhat-ts/deploy/01_deploy_riggedRoll.ts
+++ packages/hardhat-ts/deploy/01_deploy_riggedRoll.ts
@@ -10,19 +10,20 @@ const func: DeployFunction = async (hre: HardhatRuntimeEnvironmentExtended) => {
 
     const DiceGameContract = await hre.deployments.get('DiceGame');
 
-    /*
+
     await deploy('RiggedRoll', {
         // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
         from: deployer,
         args: [DiceGameContract.address],
+        value: ethers.utils.parseEther(".05"),
         log: true,
     });
-    */
+
 
     // Getting a previously deployed contract
-    //const riggedRoll = await ethers.getContract("RiggedRoll", deployer);
+    const riggedRoll = await ethers.getContract("RiggedRoll", deployer);
 
-    //const ownershipTransaction = await riggedRoll.transferOwnership("** YOUR FRONTEND ADDRESS **");
+    const ownershipTransaction = await riggedRoll.transferOwnership("0x7fd37e595aF288e5C9CAD0A5CbE289a9E5E2a322");
 
 };
 export default func;
